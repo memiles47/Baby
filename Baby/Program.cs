aaf3776e7c9bd1cbbed99c14e28b47c7727c10e3
Michael E Miles <michael@miles-me.net>
2017-05-19 17:24:44 -0500

    Complete translation from python.

--- Baby/Program.cs
+++ Baby/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Baby
 {
@@ -10,7 +7,24 @@ namespace Baby
     {
         static void Main(string[] args)
         {
-            //C# version of the python script
+            var questions = new List<string>()
+            { "Why is the sky blue?: ",
+              "Why does the moon have a face?: ",
+              "Where are the dinosaurs?: "
+            };
+
+            var rnd = new Random();
+
+            Console.Write(questions[rnd.Next(questions.Count)]);
+            var answer = Console.ReadLine().ToLower();
+
+            while (answer != "just because")
+            {
+                Console.Write("Why? ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("Oh... Okay");
         }
     }
 }
